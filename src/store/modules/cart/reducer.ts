5cfb570ed26e142e7269275ffc16ef91a9126c71
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-07-23 09:04:09 -0300

    feat: Utilizando immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux"
+import produce from 'immer'
 import { cartConstants } from "./constants"
 import { ICartState } from "./types"
 
@@ -10,10 +11,14 @@ export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case cartConstants.ADD_PRODUCT_TO_CART: {
       const { product } = action.payload
-      return {
-        ...state,
-        items: [...state.items, { product, quantity: 1 }]
-      }
+      return produce(state, (draft) => {
+        draft.items.push({ product, quantity: 1 })
+      })
+
+      // return {
+      //   ...state,
+      //   items: [...state.items, { product, quantity: 1 }]
+      // }
     }
 
     default: {

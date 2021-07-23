221480a4bd2925a701d6620714122f5395a98d1f
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-07-23 09:09:52 -0300

    feat: Aumentando quantidade

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -11,8 +11,18 @@ export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case cartConstants.ADD_PRODUCT_TO_CART: {
       const { product } = action.payload
+
       return produce(state, (draft) => {
-        draft.items.push({ product, quantity: 1 })
+
+        const productInCartIndex = draft.items.findIndex(
+          item => item.product.id === product.id
+        )
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity += 1
+        } else {
+          draft.items.push({ product, quantity: 1 })
+        }
       })
 
       // return {

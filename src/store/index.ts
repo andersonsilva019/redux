466a14b4943de38844eb4587889e1741db7e5265
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-07-23 09:26:28 -0300

    feat: Debugando com DevTools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { ICartState } from './modules/cart/types'
 import rootReducer from './modules/rootReducer'
 
@@ -6,5 +7,5 @@ export interface IState {
   cart: ICartState
 }
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeWithDevTools())
 
